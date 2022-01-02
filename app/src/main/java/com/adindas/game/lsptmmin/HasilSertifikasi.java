c9304ac3d7fcc1e19a97f46374f36747530509c2
Adindas12 <adindashabirah.as@gmail.com>
2022-01-02 15:19:16 +0700

    revisi/layout

--- app/src/main/java/com/adindas/game/lsptmmin/HasilSertifikasi.java
+++ app/src/main/java/com/adindas/game/lsptmmin/HasilSertifikasi.java
@@ -1,13 +1,10 @@
 package com.adindas.game.lsptmmin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.view.View.OnClickListener;
 import android.content.Intent;
-import android.widget.Toast;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HasilSertifikasi extends AppCompatActivity {
     Button btkembali;
@@ -17,16 +14,12 @@ public class HasilSertifikasi extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil_sertifikasi);
 
-        btkembali = (Button) findViewById(R.id.btkembali);
+        btkembali = findViewById(R.id.btkembali);
 
-        btkembali.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(About.this, MenuUtama.class);
-                finish();
-                startActivity(intent);
-            }
+        btkembali.setOnClickListener(v -> {
+            Intent intent = new Intent(HasilSertifikasi.this, MenuUtama.class);
+            finish();
+            startActivity(intent);
         });
     }
 }
